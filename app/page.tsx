fb608d4dcb449af016818426163062763a318b63
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-05-26 16:51:22 +0300

    set up project with email auth

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <>
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h1 className="h-1">Home page</h1>
+      </main>
+    </>
+  );
+}
