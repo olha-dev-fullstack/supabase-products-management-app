775592a355c080bce1411671bff9aa93c191ddde
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-06-08 16:53:43 +0300

    add images preview on edit

--- frontend/app/protected/team/[id]/products/components/edit-product-dialog.tsx
+++ frontend/app/protected/team/[id]/products/components/edit-product-dialog.tsx
@@ -1,36 +1,80 @@
 "use client";
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
-import { Label } from '@/components/ui/label';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
 import { updateProductSchema } from "../utils/zod-schema";
-import React, { useRef } from 'react'
-import { Button } from '@/components/ui/button';
-import { useProducts } from '@/hooks/use-products';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { getFileUrl } from '@/app/fileActions';
-import Image from 'next/image';
+import React, { useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { useProducts } from "@/hooks/use-products";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { getFileUrl, uploadFile } from "@/app/fileActions";
+import Image from "next/image";
 
-const EditProductDialog = ({productData, isOpen, setOpen}: {productData: any, isOpen: boolean, setOpen: (value: boolean) => void}) => {
+const EditProductDialog = ({
+  productData,
+  isOpen,
+  setOpen,
+}: {
+  productData: any;
+  isOpen: boolean;
+  setOpen: (value: boolean) => void;
+}) => {
   const formRef = useRef<HTMLFormElement>(null);
   const { editProduct } = useProducts();
   const queryClient = useQueryClient();
+  const [image, setImage] = useState(productData.imageUrl);
   const mutation = useMutation({
     mutationFn: editProduct,
     onSuccess: () => {
       queryClient.invalidateQueries(["products"]);
     },
   });
+  const imageChange = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setImage(URL.createObjectURL(e.target.files[0]));
+    }
+  };
 
-  const handleUpdateProduct = (e: FormEvent<HTMLFormElement>) => {
+  const handleFileClear = () => {
+    setImage(null);
+    const fileInput = formRef.current?.querySelector('input[name="imageFile"]');
+    if (fileInput) {
+      fileInput.value = "";
+    }
+  };
+  const handleUpdateProduct = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!formRef.current) return;
     const formData = new FormData(formRef.current);
 
+    for (const [key, value] of formData.entries()) {
+      console.log(`${key}:`, value);
+    }
+
     try {
-      const updateData = updateProductSchema.parse(formData);
-      mutation.mutate({id: productData.id, data: {...updateData}});
+      let updateData = updateProductSchema.parse(formData);
+      console.log(updateData);
+      if (updateData.imageFile) {
+        const imageUrl = await uploadFile(updateData.imageFile);
+        updateData = {
+          ...updateData,
+          imageUrl
+        }
+        console.log(imageUrl);
+        
+      }
+      mutation.mutate({ id: productData.id, data: { ...updateData } });
     } catch (error) {
       console.log(error.message);
       toast.error(error.message);
@@ -42,27 +86,42 @@ const EditProductDialog = ({productData, isOpen, setOpen}: {productData: any, is
   return (
     <Dialog open={isOpen} onOpenChange={setOpen}>
       <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Edit product</DialogTitle>
-            <DialogDescription>
-              Add data of your product here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-          <form onSubmit={handleUpdateProduct} ref={formRef}>
+        <DialogHeader>
+          <DialogTitle>Edit product</DialogTitle>
+          <DialogDescription>
+            Add data of your product here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <form onSubmit={handleUpdateProduct} ref={formRef}>
           <div className="grid gap-4">
             <div className="grid gap-3">
-              <Label htmlFor='title'>Title</Label>
-              <Input id="title" name="title" defaultValue={productData.title}/>
+              <Label htmlFor="title">Title</Label>
+              <Input id="title" name="title" defaultValue={productData.title} />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor='description'>Description</Label>
-              <Textarea id="description" name="description" defaultValue={productData.description}/>
+              <Label htmlFor="description">Description</Label>
+              <Textarea
+                id="description"
+                name="description"
+                defaultValue={productData.description}
+              />
             </div>
-            {productData.imageUrl && <Image width={100} height={100} src={productData.imageUrl} alt="Product image"/>}
+            {image && (
+              <div className="flex gap-2">
+                <Image
+                  width={100}
+                  height={100}
+                  src={image}
+                  alt="Product image"
+                />
+                <Button variant="destructive" onClick={handleFileClear}>
+                  Remove
+                </Button>
+              </div>
+            )}
             <div className="grid gap-3">
-              <Label htmlFor="img">Image</Label>
-              <Input id="img" name="img" type="file"/>
+              <Label htmlFor="imageFile">Image</Label>
+              <Input id="imageFile" name="imageFile" type="file" onChange={imageChange}/>
             </div>
           </div>
           <DialogFooter>
@@ -71,10 +130,10 @@ const EditProductDialog = ({productData, isOpen, setOpen}: {productData: any, is
             </DialogClose>
             <Button type="submit">Save changes</Button>
           </DialogFooter>
-          </form>
-        </DialogContent>
+        </form>
+      </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
-export default EditProductDialog
+export default EditProductDialog;
--- frontend/app/protected/team/[id]/products/utils/zod-schema.ts
+++ frontend/app/protected/team/[id]/products/utils/zod-schema.ts
@@ -10,5 +10,5 @@ export const updateProductSchema = zfd.formData({
     title: zfd.text(z.string().min(1).max(150)).optional(),
     status: zfd.text(z.enum(["Active", "Draft"])).optional(),
     description: zfd.text(z.string().max(250)).optional(),
-    imageUrl: zfd.text(z.url()).optional()
+    imageFile: zfd.file(z.instanceof(File).optional())
 })
